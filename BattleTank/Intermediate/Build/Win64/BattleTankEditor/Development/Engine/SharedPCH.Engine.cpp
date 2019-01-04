c51cfbba4877997bd0b2ac246f835d94666c4aa7
joshua3403 <joshua3403@gmail.com>
2019-01-04 15:12:54 +0900

    BT21 - Using override and virtual
    Redefinited beginplay()

---
+++ BattleTank/Intermediate/Build/Win64/BattleTankEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "E:/Unreal4/Section_04/BattleTank-1/BattleTank/BattleTank/Intermediate/Build/Win64/BattleTankEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank was not possessed"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Tank was possessed : %s"), *ControlledTank->GetName());
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
