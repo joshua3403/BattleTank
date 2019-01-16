75cee79017b493f523cd85ebc2c2330adbf3c8e2
joshua3403 <joshua3403@gmail.com>
2019-01-16 17:57:07 +0900

    BT36 - Make Barrel Elevate
    Used FMath::Clamp() Method to elevate barrels

--- BattleTank/Intermediate/Build/Win64/UE4Editor/Inc/BattleTank/BattleTank.init.gen.cpp
+++ BattleTank/Intermediate/Build/Win64/UE4Editor/Inc/BattleTank/BattleTank.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeBattleTank_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/BattleTank",
 				PKG_CompiledIn | 0x00000000,
-				0x47F76BE7,
-				0x77AB35D0,
+				0x6D065F11,
+				0x19EC3298,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
