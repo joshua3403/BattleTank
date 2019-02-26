044561822dc35402b2c6e67c8474e1623537bdc9
joshua3403 <joshua3403@gmail.com>
2019-02-26 21:09:16 +0900

    BT63 - Add AIMortar
    We reused our component and add AIMoratars

--- BattleTank/Source/BattleTank/Private/TankAmingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAmingComponent.cpp
@@ -106,11 +106,11 @@ void UTankAmingComponent::MoveBarrelTowards(FVector AimDirection)
 
 	// Always yaw the shortest way
 	Barrel->Elevate(DeltaRotator.Pitch);
-	if (DeltaRotator.Yaw < 180)
+	if (FMath::Abs(DeltaRotator.Yaw) < 180)
 	{
 		Turret->Rotate(DeltaRotator.Yaw);
 	}
-	else
+	else // Avoid going the long-way round
 	{
 		Turret->Rotate(-DeltaRotator.Yaw);
 	}
